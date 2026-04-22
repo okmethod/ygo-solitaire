d5e8a631d7ceaca8d1244d06879c067e46e6a667
okmethod <okmethod2020@gmail.com>
2026-04-22 18:26:14 +0900

    :recycle: use Portal

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import "../app.css";
   import { onMount } from "svelte";
-  import { Toast } from "@skeletonlabs/skeleton-svelte";
+  import { Portal, Toast } from "@skeletonlabs/skeleton-svelte";
   import Icon from "@iconify/svelte";
   import AudioToggle from "$lib/presentation/components/buttons/AudioToggle.svelte";
   import SettingsModal from "$lib/presentation/components/modals/SettingsModal.svelte";
@@ -26,18 +26,20 @@
   <title>Yu-Gi-Oh! ソリティア</title>
 </svelte:head>
 
-<Toast.Group {toaster} class="fixed top-4 right-4 z-50 flex flex-col gap-2">
-  {#snippet children(toast)}
-    <Toast {toast} class="card w-64 rounded-lg p-4 shadow-lg md:w-96">
-      <div class="flex w-full items-center gap-2">
-        <Toast.Title class="flex-1">{toast.title}</Toast.Title>
-        <Toast.CloseTrigger class="btn-icon btn-icon-sm preset-tonal ml-auto shrink-0">
-          <Icon icon="mdi:close" class="size-4" />
-        </Toast.CloseTrigger>
-      </div>
-    </Toast>
-  {/snippet}
-</Toast.Group>
+<Portal>
+  <Toast.Group {toaster} class="fixed top-4 right-4 z-50 flex flex-col gap-2">
+    {#snippet children(toast)}
+      <Toast {toast} class="card w-64 rounded-lg p-4 shadow-lg md:w-96">
+        <div class="flex w-full items-center gap-2">
+          <Toast.Title class="flex-1">{toast.title}</Toast.Title>
+          <Toast.CloseTrigger class="btn-icon btn-icon-sm preset-tonal ml-auto shrink-0">
+            <Icon icon="mdi:close" class="size-4" />
+          </Toast.CloseTrigger>
+        </div>
+      </Toast>
+    {/snippet}
+  </Toast.Group>
+</Portal>
 
 {#if isLoaded}
   <header class="bg-surface-100-900 p-2 shadow-md sm:p-4">
